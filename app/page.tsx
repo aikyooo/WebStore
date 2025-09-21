2a16c3a7d7c8cb5f8035619381423e57d4edf208
aikyooo <hrazhdanvladyslav621@gmail.com>
2025-09-21 23:10:59 +0200

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to the Webstore</h1>
+    </main>
+  );
+}
